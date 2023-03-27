004a33ae7839e76dc433d4b0f8e78eb08a3c1387
Martynas Gulbinavicius <martynass98@gmail.com>
2023-03-27 16:00:40 +0300

    v1.1

--- v1.1.py
+++ v1.1.py
@@ -1,12 +1,16 @@
-def tikrinam_ar_laimejo(sarasas,move):
-    if move == 9:
-        return False
-    for combination in [(0,6,12), (2,8,14), (4,10,16),
-                        (0,2,4), (6,8,10), (12,14,16),
-                        (0,8,16),(4,8,12)]:
-        if sarasas[combination[0]] == sarasas[combination[1]] == sarasas[combination[2]]:
+def tikrinam_ar_laimejo(sarasas,move,zaidejas):
+    if move !=9:
+        for combination in [(0, 6, 12), (2, 8, 14), (4, 10, 16),
+                            (0, 2, 4), (6, 8, 10), (12, 14, 16),
+                            (0, 8, 16), (4, 8, 12)]:
+            if sarasas[combination[0]] == sarasas[combination[1]] == sarasas[combination[2]]:
+                print("Laimejote", zaidejas)
+                return dar_karta()
 
-            return True
+    elif move == 9:   #todo kad jeigu visi
+        print('lygiosios')
+        return dar_karta()
+
 
 def ivestis(zaidejas):
     while True:
@@ -22,21 +26,34 @@ def ivestis(zaidejas):
 
 def pakeitimas (ivedimas,sarasas,zaidejas):
      return sarasas.replace(str(ivedimas),zaidejas)
-def zaidimas(sarasas):
+
+def dar_karta():
+    klausimas = input('Ar norite zaisti dar karta?\n Iveskite T/N ')
+    atsakymas = klausimas.upper()
+    if atsakymas == 'T':
+        move = 0
+        return True
+    elif atsakymas == 'N':
+        print('Aciu uz zaidima')
+        return False
+
+def zaidimas():
     move = 0
     zaidejas = 'X'
+    sarasas = ('7 8 9\n4 5 6\n1 2 3')
     print(sarasas)
     while True:
         ivedimas = ivestis(zaidejas)
         move += 1
         sarasas = pakeitimas(ivedimas, sarasas, zaidejas)
         print(sarasas)
-        if tikrinam_ar_laimejo(sarasas,move) is True:
-            print("Laimejote", zaidejas)
+        if tikrinam_ar_laimejo(sarasas,move,zaidejas) is True:
+            sarasas = ('7 8 9\n4 5 6\n1 2 3')
+            move = 0
+
+        elif tikrinam_ar_laimejo(sarasas,move,zaidejas) is False:
             break
-        elif tikrinam_ar_laimejo(sarasas,move) is False:
-            print('Lygiosios')
-            break
+
 
 
         if zaidejas == 'X':
@@ -46,7 +63,9 @@ def zaidimas(sarasas):
 
 
 
-Sarasas = ('7 8 9\n4 5 6\n1 2 3')
-
-
-zaidimas(Sarasas)
+zaidimas()
+# move = 0
+# zaidejas = 'X'
+# sarasas = ('1 8 9\n1 5 6\n1 2 3')
+#
+# tikrinam_ar_laimejo(sarasas,move,zaidejas)
